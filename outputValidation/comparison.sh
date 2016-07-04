cf2766420b3e357dc75f1bdd45b52e9fe8237dce
Pieter <pmoris@users.noreply.github.com>
2016-07-04 17:24:29 +0200

    Make output validation script generate a diff log.
    Diff log compares the output of the BEAUti created tip sampling .xml files and the ones generated by the python tool.

--- outputValidation/comparison.sh
+++ outputValidation/comparison.sh
@@ -8,16 +8,26 @@ do
 	fi
 
 	if grep -q realspace <<<${name}; then
-		python ../sample-tip-dates-in-BEAUti-xml.py ./templates/02-tip-dates-no-prior.xml --sequences restrictedSequenceList.txt --output validationOutput/updated-log-normal-realspace.xml -d log-normal --realspace
+		python ../sample-tip-dates-in-BEAUti-xml.py ./templates/02-tip-dates-no-prior.xml --sequences restrictedSequenceList.txt --output templates-updated/updated-${name} -d log-normal --realspace
 	    continue
 	fi
 
 	if grep -q '1x' <<<${name}; then
-	    python ../sample-tip-dates-in-BEAUti-xml.py ./templates/02-tip-dates-no-prior.xml --sequences restrictedSequenceList.txt --output validationOutput/updated-${name} -d 1/x
+	    python ../sample-tip-dates-in-BEAUti-xml.py ./templates/02-tip-dates-no-prior.xml --sequences restrictedSequenceList.txt --output templates-updated/updated-${name} -d 1/x
 	    continue
 	fi
 
 	dist=$(echo ${name} | sed -r 's/[0-9]{2}-([[:alnum:]-]*).xml/\1/g')
-	python ../sample-tip-dates-in-BEAUti-xml.py ./templates/02-tip-dates-no-prior.xml --sequences restrictedSequenceList.txt --output validationOutput/updated-${name} -d ${dist}
+	python ../sample-tip-dates-in-BEAUti-xml.py ./templates/02-tip-dates-no-prior.xml --sequences restrictedSequenceList.txt --output templates-updated/updated-${name} -d ${dist}
 
+done
+
+cd templates-updated
+
+echo '' > diff-output.txt
+
+for i in *.xml
+do
+	echo -e "\n\n\n\n${i#*-}\n\n" >> diff-output.txt
+	diff ../templates/${i#*-} ${i} >> diff-output.txt
 done
